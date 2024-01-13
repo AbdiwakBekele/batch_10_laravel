40ffc7aef60e3dd70f8c0dd5d19150e547472dd0
amantth5 <amantarekegn5@gmail.com>
2024-01-13 11:13:32 +0300

    edit page created

---
+++ resources/views/edit_product.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="edit_page">
+    <h2>Product edit</h2>
+    <form action="">
+Product name<input type="text">
+Product description<input type="text">
+Product price<input type="text">
+
+
+
+    </form>
+</div>
+    
+</body>
+</html>
